c35ed43c871212e1505c6009ad9605e05c12c885
DmitryDzz <dzakhovds@gmail.com>
2015-03-25 00:56:42 +0300

    Asynchronous SmoothStepper library

--- stepperbot/smoothstepper.h
+++ stepperbot/smoothstepper.h
@@ -1,36 +1,43 @@
 #ifndef SmoothStepper_h
 #define SmoothStepper_h
 
-// library interface description
+enum StepperMode {
+  FULL_STEP = 1,
+  HALF_STEP = 2,
+  QUARTER_STEP = 4
+};
+
 class SmoothStepper {
   public:
-    SmoothStepper(int number_of_full_steps, int substeps_in_step, int motor_pin_1, int motor_pin_2, int motor_pin_3, int motor_pin_4);
+    SmoothStepper(int motorFullStepsNumber, StepperMode stepperMode, int motorPinA1, int motorPinA2, int motorPinB1, int motorPinB2);
 
-    void setSpeed(long whatSpeed);
-
-    void step(int number_of_full_steps);
+    void zero();
+    void start(long period, long substeps, long millis);
+    void stop();
+    void update(long millis);
 
   private:
-    void stepMotor(int this_substep);
-    
-    int direction;        // Direction of rotation
-    int speed;          // Speed in RPMs
-    unsigned long substep_delay;    // delay between steps, in ms, based on speed
-    int number_of_substeps;
-    int substeps_in_step;
-    int substep_number;        // which step the motor is on
+    bool started;
+    int motorFullStepsNumber;
+    StepperMode stepperMode;
+
+    signed short direction;
+    long substepDelay;
+    long lastSubstepTime;
+    long substeps;
+    long currentSubstep;
     
     // motor pin numbers:
-    int motor_pin_1;
-    int motor_pin_2;
-    int motor_pin_3;
-    int motor_pin_4;
+    int motorPinA1;
+    int motorPinA2;
+    int motorPinB1;
+    int motorPinB2;
     
-    long last_substep_time;      // time stamp in ms of when the last step was taken
+    unsigned short HIGH_ANALOG;
+    unsigned short LOW_ANALOG;
+    unsigned short MID_ANALOG;
     
-    unsigned short HIGH_A;
-    unsigned short LOW_A;
-    unsigned short MID_A;
+    void updateMotor(long phase);
 };
 
 #endif
--- stepperbot/stepperbot.ino
+++ stepperbot/stepperbot.ino
@@ -3,28 +3,21 @@
 #define stepsPerRevolution 20    // steps per revolution (limited to 315°)
 #define LED_PIN 13
 
-SmoothStepper stepper(stepsPerRevolution, 2, 2, 3, 4, 5);
+SmoothStepper stepper(stepsPerRevolution, FULL_STEP, 2, 3, 4, 5);
 
 void setup() {
+  Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
-  stepper.setSpeed(1);    // set the motor speed to 30 RPM (360 PPS aprox.).
 
-  testMotor();
-  testMotor();
-  testMotor();
+  digitalWrite(LED_PIN, HIGH);
+  stepper.zero();
+  delay(3000);
+  digitalWrite(LED_PIN, LOW);
+
+//  stepper.start(5000, 3, millis());
 }
 
 void loop() {
+  stepper.update(millis());
 }
 
-void testMotor() {
-  // step one revolution  in one direction:
-  digitalWrite(LED_PIN, HIGH);
-  stepper.step(stepsPerRevolution);
-  delay(1000);
-  
-   // step one revolution in the other direction:
-  digitalWrite(LED_PIN, LOW);
-  stepper.step(-stepsPerRevolution);
-  delay(1000); 
-}  
