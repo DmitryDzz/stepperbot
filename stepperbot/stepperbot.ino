62ee88cbebc984bcf90a11225afa2cc97e98a272
DmitryDzz <dzakhovds@gmail.com>
2015-03-29 20:11:11 +0300

    Demo sketch fix

--- stepperbot/stepperbot.ino
+++ stepperbot/stepperbot.ino
@@ -3,8 +3,11 @@
 #define stepsPerRevolution 20    // steps per revolution (limited to 315°)
 #define LED_PIN 13
 
-SmoothStepper stepper(stepsPerRevolution, QUARTER_STEP, 2, 3, 4, 5);
-//SmoothStepper stepper(stepsPerRevolution, HALF_STEP, 2, 3, 4, 5);
+//const StepperMode STEPPER_MODE = FULL_STEP;
+//const StepperMode STEPPER_MODE = HALF_STEP;
+const StepperMode STEPPER_MODE = QUARTER_STEP;
+
+SmoothStepper stepper(stepsPerRevolution, STEPPER_MODE, 2, 3, 4, 5);
 
 void setup() {
   Serial.begin(9600);
@@ -17,8 +20,6 @@ void setup() {
   stepper.zero(50);
   delay(1000);
   digitalWrite(LED_PIN, LOW);
-
-//  stepper.start(100, 30, millis());
 }
 
 long period = 4000;
@@ -26,13 +27,12 @@ long lastTimestamp = 0;
 long clockwise = 1;
 
 void loop() {
-//  stepper.update(millis());
   long timestamp = millis();
   
   stepper.update(timestamp);
 
   if (timestamp - lastTimestamp >= period) {
-    stepper.start(1000, 120 * clockwise, timestamp);
+    stepper.start(1000, 60 * STEPPER_MODE * clockwise, timestamp); // 3 revolutions
     clockwise *= -1;
     lastTimestamp = timestamp;
   }
